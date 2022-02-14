bd4f49514fb6806cc07b8d6c6cfeb823bb9e5462
Igor Shlyakhtenkov <ishlyakhtenkov@yandex.ru>
2022-02-14 17:23:00 +0300

    Add Email Verification Web Service module

---
+++ email-verification-service/src/main/java/ru/javaprojects/emailverificationservice/EmailVerificationServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.javaprojects.emailverificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailVerificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailVerificationServiceApplication.class, args);
+    }
+
+}
---
+++ email-verification-service/src/test/java/ru/javaprojects/emailverificationservice/EmailVerificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javaprojects.emailverificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailVerificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
