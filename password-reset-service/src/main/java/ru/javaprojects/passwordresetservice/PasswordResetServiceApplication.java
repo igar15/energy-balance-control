8ddb7fbdb9973afb14c3365cac26e4ee78156817
Igor Shlyakhtenkov <ishlyakhtenkov@yandex.ru>
2022-02-18 15:30:28 +0300

    Add Password Reset Web Service module

---
+++ password-reset-service/src/main/java/ru/javaprojects/passwordresetservice/PasswordResetServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.javaprojects.passwordresetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordResetServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasswordResetServiceApplication.class, args);
+    }
+
+}
---
+++ password-reset-service/src/test/java/ru/javaprojects/passwordresetservice/PasswordResetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javaprojects.passwordresetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordResetServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
