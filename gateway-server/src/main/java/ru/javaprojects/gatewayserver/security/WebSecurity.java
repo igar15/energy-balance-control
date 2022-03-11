861d42bcedf2696a1f2e8876818c2dc708969837
Igor Shlyakhtenkov <ishlyakhtenkov@yandex.ru>
2022-03-11 18:25:38 +0300

    Add CORS to Gateway server

--- gateway-server/src/main/java/ru/javaprojects/gatewayserver/security/WebSecurity.java
+++ gateway-server/src/main/java/ru/javaprojects/gatewayserver/security/WebSecurity.java
@@ -7,7 +7,13 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
@@ -19,6 +25,8 @@ public class WebSecurity {
 
     @Value("${gateway-server.free-paths}")
     private String[] freePaths;
+    @Value("${gateway-server.allowed-origins}")
+    private List<String> allowedOrigins;
     private final AuthenticationManager authenticationManager;
     private final SecurityContextRepository securityContextRepository;
 
@@ -27,11 +35,11 @@ public class WebSecurity {
         this.securityContextRepository = securityContextRepository;
     }
 
-    // TODO ADD CORS
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         return http
                 .csrf().disable()
+                .cors().and()
                 .httpBasic().disable()
                 .formLogin().disable()
                 .logout().disable()
@@ -45,5 +53,21 @@ public class WebSecurity {
                 .authenticationEntryPoint(((exchange, e) -> Mono.fromRunnable(() -> exchange.getResponse().setStatusCode(UNAUTHORIZED))))
                 .accessDeniedHandler(((exchange, e) -> Mono.fromRunnable(() -> exchange.getResponse().setStatusCode(FORBIDDEN))))
                 .and().build();
+    }
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(allowedOrigins);
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+                "Accept", "Authorization-Token", "Authorization", "Origin, Accept", "X-Requested-With",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization-Token", "Authorization",
+                "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(urlBasedCorsConfigurationSource);
     }
 }
