d7b4b115fd8838f068624b7727019776c9ee75bf
Igor Shlyakhtenkov <ishlyakhtenkov@yandex.ru>
2022-03-12 14:33:55 +0300

    Change Gateway server security config

--- gateway-server/src/main/java/ru/javaprojects/gatewayserver/security/WebSecurity.java
+++ gateway-server/src/main/java/ru/javaprojects/gatewayserver/security/WebSecurity.java
@@ -3,6 +3,7 @@ package ru.javaprojects.gatewayserver.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -47,6 +48,7 @@ public class WebSecurity {
                 .securityContextRepository(securityContextRepository)
                 .authorizeExchange()
                 .pathMatchers(freePaths).permitAll()
+                .pathMatchers(HttpMethod.OPTIONS).permitAll()
                 .pathMatchers("/actuator/*").hasRole("ADMIN")
                 .anyExchange().authenticated()
                 .and().exceptionHandling()
